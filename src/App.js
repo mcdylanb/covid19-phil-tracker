bd2391a19aaa47a6dc60ff01ac8f41e136c69ed4
mcdylanb <dylansatgalab@gmail.com>
2020-06-18 17:32:57 +0800

    Puts data in respective categories

--- src/App.js
+++ src/App.js
@@ -7,18 +7,34 @@ const url = "https://covid19.mathdro.id/api/countries/Philippines";
 
 const App = () => {
   const [confirmed, setConfirmed] = useState([]);
+  const [recovered, setRecovered] = useState([]);
+  const [deaths, setDeaths] = useState([]);
 
   axios.get(url).then(response => {
     console.log("promise fulfilled");
     setConfirmed(response.data.confirmed);
+    setRecovered(response.data.recovered);
+    setDeaths(response.data.deaths);
   });
 
   console.log(`confirmed data : ${confirmed.value}`);
+  console.log(`confirmed data : ${recovered.value}`);
+  console.log(`confirmed data : ${deaths.value}`);
 
   return (
     <div>
-      <h3>Confirmed</h3>
-      <h1> {confirmed.value}</h1>
+      <div>
+        <h3>Confirmed</h3>
+        <h1> {confirmed.value}</h1>
+      </div>
+      <div>
+        <h3>Recovered</h3>
+        <h1> {recovered.value}</h1>
+      </div>
+      <div>
+        <h3>Deaths</h3>
+        <h1> {deaths.value}</h1>
+      </div>
     </div>
   );
 };
