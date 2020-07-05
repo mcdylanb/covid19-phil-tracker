de933cd7c58d187c48464a3ce47866407147b6e6
mcdylanb <dylansatgalab@gmail.com>
2020-07-05 18:21:59 +0800

    Uses muithem, createMuiTheme and ThemeProvider

--- src/components/CardData.js
+++ src/components/CardData.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { Card, Typography } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider
+} from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  root: {
+    "background-color": "#3824B8"
+  }
+});
+
+const theme = createMuiTheme({
+  palette: {
+    text: {
+      secondary: "#fff"
+    }
+  }
+});
 const CardData = ({ name, value }) => {
+  const classes = useStyles();
   return (
-    <div className="CardData">
-      <Card className="card-root">
-        <CardContent>
-          <Typography variant="h5">{name}</Typography>
-          <Typography variant="h2">{value}</Typography>
-        </CardContent>
-      </Card>
+    <div>
+      <ThemeProvider theme={theme}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography variant="h5" color="textSecondary">
+              {name}
+            </Typography>
+            <Typography variant="h2" color="textSecondary">
+              {value}
+            </Typography>
+          </CardContent>
+        </Card>
+      </ThemeProvider>
     </div>
   );
 };
