ffd56f037e84e5e9f56abcef2589ebcf41fe0baf
mcdylanb <dylansatgalab@gmail.com>
2020-07-09 10:44:08 +0800

    Adjusts struture by using nested Grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CardData from "./components/CardData.js";
 import { Grid, Typography, Button } from "@material-ui/core";
+import "./App.css";
 
 const url = "https://covid19.mathdro.id/api/countries/Philippines";
 
@@ -10,6 +11,8 @@ const App = () => {
   const [recovered, setRecovered] = useState("");
   const [deaths, setDeaths] = useState("");
 
+  console.log(confirmed);
+  console.log(confirmed.value);
   useEffect(() => {
     axios.get(url).then(response => {
       setConfirmed(response.data.confirmed);
@@ -22,62 +25,96 @@ const App = () => {
     <div>
       <Grid
         container
+        direction="column"
         spacing={2}
         alignItems="center"
         justify="center"
-        style={{ minHeight: "100vh" }}
       >
-        <Grid item xs={10}>
-          {/* intro text */}
-          <Grid container>
-            <Grid item>
-              <Typography variant="h4" align="center">
-                Lets Help
-              </Typography>
-              <Typography variant="h4" align="center">
-                Recover The Earth
-              </Typography>
-              <Typography variant="h4" align="center">
-                Start From Us!
-              </Typography>
-            </Grid>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          direction="column"
+          className="intro"
+          item
+          xs={10}
+        >
+          <Grid item xs={10}>
+            {/* intro text */}
+            <Typography variant="h4" align="center">
+              Lets Help
+            </Typography>
+            <Typography variant="h4" align="center">
+              Recover The Earth
+            </Typography>
+            <Typography
+              variant="h4"
+              align="center"
+              className="intro__important"
+            >
+              Start From Us!
+            </Typography>
           </Grid>
+        </Grid>
 
-          {/* additional text */}
-          <Grid container>
-            <Grid item>
-              <Typography>
-                The outbreak of the coronavirus issue or called Covid-19 makes
-                the Earth grieve
-              </Typography>
-            </Grid>
+        {/* additional text */}
+        <Grid
+          container
+          className="intro"
+          item
+          alignItems="center"
+          justify="center"
+          xs={8}
+        >
+          <Grid item>
+            <Typography>
+              The outbreak of the coronavirus issue or called Covid-19 makes the
+              Earth grieve
+            </Typography>
           </Grid>
+        </Grid>
 
-          {/* action buttons */}
-          <Grid container spacing={2} alignItems="center" justify="center">
-            <Grid item xs={6}>
-              <Button variant="contained" color="primary">
-                Learn More
-              </Button>
-            </Grid>
-            <Grid item xs={6}>
-              <Button variant="outlined" color="secondary">
-                Watch Video
-              </Button>
-            </Grid>
+        {/* action buttons */}
+        <Grid
+          container
+          item
+          alignItems="center"
+          justify="center"
+          direction="row"
+          xs={8}
+        >
+          <Grid item xs={6}>
+            <Button variant="contained" color="primary">
+              Learn More
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="outlined" color="secondary">
+              Watch Video
+            </Button>
           </Grid>
         </Grid>
 
         {/* stats */}
-        <Grid item xs={8}>
-          <CardData name=" Confirmed " value={confirmed.value} />
-        </Grid>
+        <Grid
+          container
+          direction="column"
+          spacing={2}
+          alignItems="center"
+          justify="center"
+          item
+          xs={10}
+        >
+          <Grid item xs={12}>
+            <CardData name=" Confirmed " value={confirmed.value} />
+          </Grid>
 
-        <Grid item xs={8}>
-          <CardData name=" Recovered " value={recovered.value} />
-        </Grid>
-        <Grid item xs={8}>
-          <CardData name=" Deaths " value={deaths.value} />
+          <Grid item xs={12}>
+            <CardData name=" Recovered " value={recovered.value} />
+          </Grid>
+          <Grid item xs={12}>
+            <CardData name=" Deaths " value={deaths.value} />
+          </Grid>
         </Grid>
       </Grid>
     </div>
